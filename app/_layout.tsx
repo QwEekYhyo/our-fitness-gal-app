7d5b2bd93677f4828ddd270a4d5daa235309579b
Logan Lucas <logan.lucas83@gmail.com>
2025-03-10 10:25:49 +0100

    add done text in active workout header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,27 @@
-import { Stack } from "expo-router";
+import { Stack, useNavigation } from "expo-router";
+import { Pressable, Text } from "react-native";
 
 function RootLayout() {
+    const navigation = useNavigation();
+
     return (
         <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen
+                name="workout-session"
+                options={{
+                    title: "Active Workout",
+                    headerTitleAlign: "center",
+                    headerRight: () => (
+                        <Pressable
+                            style={{ justifyContent: "center", alignItems: "flex-end" }}
+                            onPress={navigation.goBack}
+                        >
+                            <Text style={{ fontSize: 16 }}>Done</Text>
+                        </Pressable>
+                    ),
+                }}
+            />
         </Stack>
     );
 }
