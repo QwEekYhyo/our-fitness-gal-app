d28f61d8cc44aeb5b097ba532422b66e9f2f3939
Logan Lucas <logan.lucas83@gmail.com>
2025-03-13 10:28:41 +0100

    start text inputs in active workout, I need to make it dynamic now

--- components/OnGoingExercise.tsx
+++ components/OnGoingExercise.tsx
@@ -1,6 +1,6 @@
-import { Text, TextProps, View } from "react-native";
+import { Text, TextInput, TextInputProps, TextProps, View } from "react-native";
 
-import { onGoingExerciseStyle as style } from "@/styles/OnGoingExercise";
+import { onGoingExerciseStyle as componentStyle } from "@/styles/OnGoingExercise";
 
 export interface OnGoingExerciseProps {
     exerciseName: string;
@@ -13,28 +13,43 @@ interface CustomTextProps extends TextProps {
 function CustomText({ children, style, header, ...props }: CustomTextProps): React.ReactNode {
     const color = header ? { color: "grey" } : {};
     return (
-        <Text style={[color, { fontSize: 20 }, style]} {...props}>
+        <Text style={[color, componentStyle.customText, style]} {...props}>
             {children}
         </Text>
     );
 }
 
+function CustomTextInput({ style, ...props }: TextInputProps): React.ReactNode {
+    return (
+        <TextInput
+            autoComplete="off"
+            inputMode="decimal"
+            placeholderTextColor="grey"
+            style={[componentStyle.CustomTextInput, style]}
+            {...props}
+        />
+    );
+}
+
 function OnGoingExercise(props: OnGoingExerciseProps): React.ReactNode {
     return (
-        <View style={style.container}>
-            <CustomText style={style.exerciseName}>{props.exerciseName}</CustomText>
-            <View style={style.setsGrid}>
-                <View style={style.setsColumn}>
+        <View style={componentStyle.container}>
+            <CustomText style={componentStyle.exerciseName}>{props.exerciseName}</CustomText>
+            <View style={componentStyle.setsGrid}>
+                <View style={componentStyle.setsColumn}>
                     <CustomText header>Set</CustomText>
                     <CustomText>1</CustomText>
+                    <CustomText>2</CustomText>
                 </View>
-                <View style={style.setsColumn}>
+                <View style={componentStyle.setsColumn}>
                     <CustomText header>KG</CustomText>
                     <CustomText>100</CustomText>
+                    <CustomTextInput placeholder="90" />
                 </View>
-                <View style={style.setsColumn}>
+                <View style={componentStyle.setsColumn}>
                     <CustomText header>Reps</CustomText>
                     <CustomText>3</CustomText>
+                    <CustomTextInput placeholder="6" inputMode="numeric" />
                 </View>
             </View>
         </View>
--- styles/OnGoingExercise.ts
+++ styles/OnGoingExercise.ts
@@ -2,19 +2,30 @@ import { StyleSheet } from "react-native";
 
 export const onGoingExerciseStyle = StyleSheet.create({
     container: {
-        width: "70%",
+        width: "80%",
         marginTop: 30,
     },
     exerciseName: {
         fontSize: 25,
         fontWeight: 700,
         marginBottom: 10,
+        textAlign: "left",
+    },
+    customText: {
+        fontSize: 20,
+        textAlign: "center",
+    },
+    CustomTextInput: {
+        width: "100%",
+        fontSize: 20,
+        textAlign: "center",
     },
     setsGrid: {
         flexDirection: "row",
         justifyContent: "space-around",
     },
     setsColumn: {
+        flex: 1,
         flexDirection: "column",
         gap: 10,
     },
