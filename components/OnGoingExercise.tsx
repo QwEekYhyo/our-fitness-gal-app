63d251ad61636337356bd2c7e7c8b315bc56189c
Logan Lucas <logan.lucas83@gmail.com>
2025-03-13 11:29:50 +0100

    dynamically add rows for active workout exercise

--- components/OnGoingExercise.tsx
+++ components/OnGoingExercise.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Text, TextInput, TextInputProps, TextProps, View } from "react-native";
 
 import { onGoingExerciseStyle as componentStyle } from "@/styles/OnGoingExercise";
@@ -10,7 +11,7 @@ interface CustomTextProps extends TextProps {
     header?: boolean;
 }
 
-function CustomText({ children, style, header, ...props }: CustomTextProps): React.ReactNode {
+function CustomText({ children, style, header, ...props }: CustomTextProps) {
     const color = header ? { color: "grey" } : {};
     return (
         <Text style={[color, componentStyle.customText, style]} {...props}>
@@ -19,37 +20,73 @@ function CustomText({ children, style, header, ...props }: CustomTextProps): Rea
     );
 }
 
-function CustomTextInput({ style, ...props }: TextInputProps): React.ReactNode {
+function CustomTextInput({ style, onChangeText, ...props }: TextInputProps) {
     return (
         <TextInput
             autoComplete="off"
             inputMode="decimal"
             placeholderTextColor="grey"
             style={[componentStyle.CustomTextInput, style]}
+            onChangeText={onChangeText}
             {...props}
         />
     );
 }
 
 function OnGoingExercise(props: OnGoingExerciseProps): React.ReactNode {
+    const [sets, setSets] = useState([{ kg: "", reps: "" }]);
+
+    const handleInputChange = (index: number, field: "kg" | "reps", value: string) => {
+        const updatedSets = [...sets];
+        updatedSets[index] = { ...updatedSets[index], [field]: value };
+
+        if (
+            updatedSets.length > 1 &&
+            updatedSets[index].kg === "" &&
+            updatedSets[index].reps === ""
+        ) {
+            updatedSets.splice(index, 1);
+        }
+
+        setSets(updatedSets);
+
+        if (index === sets.length - 1 && updatedSets[index].kg && updatedSets[index].reps) {
+            setSets([...updatedSets, { kg: "", reps: "" }]);
+        }
+    };
+
     return (
         <View style={componentStyle.container}>
             <CustomText style={componentStyle.exerciseName}>{props.exerciseName}</CustomText>
             <View style={componentStyle.setsGrid}>
                 <View style={componentStyle.setsColumn}>
                     <CustomText header>Set</CustomText>
-                    <CustomText>1</CustomText>
-                    <CustomText>2</CustomText>
+                    {sets.map((_, i) => (
+                        <CustomText key={`set-${i}`}>{i + 1}</CustomText>
+                    ))}
                 </View>
                 <View style={componentStyle.setsColumn}>
                     <CustomText header>KG</CustomText>
-                    <CustomText>100</CustomText>
-                    <CustomTextInput placeholder="90" />
+                    {sets.map((set, i) => (
+                        <CustomTextInput
+                            key={`kg-${i}`}
+                            placeholder="100"
+                            value={set.kg}
+                            onChangeText={(value) => handleInputChange(i, "kg", value)}
+                        />
+                    ))}
                 </View>
                 <View style={componentStyle.setsColumn}>
                     <CustomText header>Reps</CustomText>
-                    <CustomText>3</CustomText>
-                    <CustomTextInput placeholder="6" inputMode="numeric" />
+                    {sets.map((set, i) => (
+                        <CustomTextInput
+                            key={`reps-${i}`}
+                            placeholder="3"
+                            inputMode="numeric"
+                            value={set.reps}
+                            onChangeText={(value) => handleInputChange(i, "reps", value)}
+                        />
+                    ))}
                 </View>
             </View>
         </View>
